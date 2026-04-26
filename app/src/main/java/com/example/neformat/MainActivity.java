94c61178a48159dd2750b9f3a78eba349990b73c
konyshev.aleksandr <aleksandr.konyshev@vkteam.ru>
2026-04-26 20:41:59 +0300

    clean project

---
+++ app/src/main/java/com/example/neformat/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.neformat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Настройка навигации
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        Window window = getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.status_bar_color));
+
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+            bottomNav.setItemHorizontalTranslationEnabled(false);
+
+            NavigationUI.setupWithNavController(bottomNav, navController);
+
+
+        }
+    }
+
+    public void openFavoritesFromOutside() {
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView); // или твой ID
+        navView.setSelectedItemId(R.id.favoritesFragment);
+    }
+
+}
+
